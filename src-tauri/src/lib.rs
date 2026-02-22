755cb07262509a9fa5a0c9534f3bb369f14311bd
David Cristóbal <cristobal.david@gmail.com>
2026-02-22 19:58:11 +0100

    chore: add tauri-plugin-dialog for file picker support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -35,6 +35,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
         .manage(AppState {
             global_config: Mutex::new(global_config),
             spaces: Mutex::new(spaces),
